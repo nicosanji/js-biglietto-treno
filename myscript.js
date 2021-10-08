e1609f5afbde4b9631d36fa8220206ae0ba4b64e
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-08 15:05:17 +0200

    START: inserite richieste per l'utente

---
+++ myscript.js
@@ -0,0 +1,20 @@
+// chiedo all'utente numero di km che vuole percorrere
+
+const kmUtente = prompt("Quanti Km devi percorrere ?");
+
+if (isNaN(kmUtente)) {   
+    alert("I Km inseriti non sono validi, devi inserire un numero !");
+}
+
+// chiedo all'utente l'età
+
+const anniUtente = prompt("Quanti anni hai ?");
+
+if (isNaN(anniUtente)) {    
+    alert("L'età inserita non è valida, devi inserire un numero !");
+}
+
+// calcolo del costo del biglietto (generico)
+
+
+
