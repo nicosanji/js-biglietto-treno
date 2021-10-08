8066c31d48d3e6f95cea6680d9ff890e018f8e36
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-08 15:50:32 +0200

    Creati if per gli sconti

--- myscript.js
+++ myscript.js
@@ -1,8 +1,8 @@
 // chiedo all'utente numero di km che vuole percorrere
 
-const kmUtente = prompt("Quanti Km devi percorrere ?");
+let kmUtente = prompt("Quanti Km devi percorrere ?");
 
-if (isNaN(kmUtente)) {   
+if (isNaN(kmUtente)) {
     alert("I Km inseriti non sono validi, devi inserire un numero !");
 }
 
@@ -10,11 +10,35 @@ if (isNaN(kmUtente)) {
 
 const anniUtente = prompt("Quanti anni hai ?");
 
-if (isNaN(anniUtente)) {    
+if (isNaN(anniUtente)) {
     alert("L'età inserita non è valida, devi inserire un numero !");
 }
 
 // calcolo del costo del biglietto (generico)
+
+let prezzoBiglietto = kmUtente * 0.21
+
+console.log(prezzoBiglietto)
+
+
+// sconto per minorenni
+
+if (anniUtente <= 18) {
+    prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto * 20 ) / 100));
+}
+
+// sconto per over 65
+
+if (anniUtente >= 65) {
+    prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto * 40 ) / 100));
+}
+
+console.log (prezzoBiglietto)
+
+// mostra all'utente il costo del biglietto in €
+
+alert("Il costo del tuo biglietto è di € " + prezzoBiglietto);
+
 
 
 
