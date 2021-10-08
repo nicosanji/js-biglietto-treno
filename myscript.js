066adc41a6a3944a34b9c769ce89e5a377d4d635
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-08 16:36:31 +0200

    DEF. 1.0: completato alert finale con prezzo e create condizioni sensate con "else if" per gli sconti e "else" per la validità

--- myscript.js
+++ myscript.js
@@ -6,38 +6,45 @@ if (isNaN(kmUtente)) {
     alert("I Km inseriti non sono validi, devi inserire un numero !");
 }
 
-// chiedo all'utente l'età
+else {
 
-const anniUtente = prompt("Quanti anni hai ?");
+    // chiedo all'utente l'età
 
-if (isNaN(anniUtente)) {
-    alert("L'età inserita non è valida, devi inserire un numero !");
+    const anniUtente = prompt("Quanti anni hai ?");
+
+    if (isNaN(anniUtente)) {
+        alert("L'età inserita non è valida, devi inserire un numero !");
+    }
+
+    else {
+
+        // calcolo del costo del biglietto (generico)
+
+        let prezzoBiglietto = kmUtente * 0.21
+
+        console.log(prezzoBiglietto)
+
+        // sconto per minorenni -20% e anziani -40%
+
+        if (anniUtente <= 18) {
+            prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto * 20) / 100));
+        }
+
+        else if (anniUtente >= 65) {
+            prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto * 40) / 100));
+        }
+
+        console.log(prezzoBiglietto)
+
+        // mostra all'utente il costo del biglietto in €
+
+        alert("Il costo del tuo biglietto è di € " + prezzoBiglietto.toFixed(2));
+
+    }
+
 }
 
-// calcolo del costo del biglietto (generico)
 
-let prezzoBiglietto = kmUtente * 0.21
-
-console.log(prezzoBiglietto)
-
-
-// sconto per minorenni
-
-if (anniUtente <= 18) {
-    prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto * 20 ) / 100));
-}
-
-// sconto per over 65
-
-if (anniUtente >= 65) {
-    prezzoBiglietto = (prezzoBiglietto - ((prezzoBiglietto * 40 ) / 100));
-}
-
-console.log (prezzoBiglietto)
-
-// mostra all'utente il costo del biglietto in €
-
-alert("Il costo del tuo biglietto è di € " + prezzoBiglietto);
 
 
 
